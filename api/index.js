5b75369d275bae14277020683a710a0c9bc1668a
Pen <103081768+techdb-tran@users.noreply.github.com>
2023-09-20 17:47:12 +0700

    Update index.js
    test deploy

--- api/index.js
+++ api/index.js
@@ -45,6 +45,9 @@ const upload = multer({storage:storage});
 app.post("/api/upload", upload.single("file"),(req,res)=>{
     res.status(200).json("File has been uploaded");
 })
+app.get("/api", (req,res)=>{
+    res.status(200).json("Hello");
+})
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
