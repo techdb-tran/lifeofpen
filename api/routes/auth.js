4f6da3263264d511fc539317f81f16ea1d5ca89f
Pen <103081768+techdb-tran@users.noreply.github.com>
2023-09-20 21:30:38 +0700

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require('bcrypt')
  //REGISTER
- router.post("/register", async (req, res)=>{
+ router.post("https://lifeofpen-api.vercel.app/register", async (req, res)=>{
     try{
         const salt = await bcrypt.genSalt(10);
         const hashedPass = await bcrypt.hash(req.body.password, salt)
@@ -18,7 +18,7 @@ const bcrypt = require('bcrypt')
     }
  });
  //LOGIN
- router.post("/login", async (req, res) => {
+ router.post("https://lifeofpen-api.vercel.app/login", async (req, res) => {
     try {
       const user = await User.findOne({ username: req.body.username });
       if (!user) return res.status(400).json("Wrong credentials!");
